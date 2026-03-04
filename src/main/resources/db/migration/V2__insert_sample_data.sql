84bf766a2c255e8de322de0f1be56d4f18d1a184
marcos <marcosregato01@gmail.com>
2026-03-04 02:38:50 -0300

    update_pacote_controller

---
+++ src/main/resources/db/migration/V2__insert_sample_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario (nome, endereco, cnpj, cpf, email, tipo) VALUES ('admin', 'Rua Principal, 123', '12345678000195', '12345678901', 'admin@email.com', 'admin');
+INSERT INTO login (fk_usuario, login, senha) VALUES (1, 'admin', 'admin');
