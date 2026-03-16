64651be5df018adc02d857d3b38e312253f3c5b2
marcos <marcosregato01@gmail.com>
2026-03-16 03:08:57 -0300

    update_v3

--- src/main/java/com/br/hermescomercial/dao/ProdutoDao.java
+++ src/main/java/com/br/hermescomercial/dao/ProdutoDao.java
@@ -7,7 +7,7 @@ package com.br.hermescomercial.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 import java.util.ArrayList;
 import java.util.List;
 
