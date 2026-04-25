1e7f98277602dc2678794b18dca5680f2cf9b362
marcos <marcosregato01@gmail.com>
2026-04-25 15:44:49 -0300

    terste

--- src/main/java/com/br/hermescomercial/MainApplication.java
+++ src/main/java/com/br/hermescomercial/MainApplication.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
